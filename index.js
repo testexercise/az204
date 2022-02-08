ea9a827910081d36dc8fa4e954a16c68a917fd82
testexercise <99247014+testexercise@users.noreply.github.com>
2022-02-08 13:02:26 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/hello', async (req, res) => {
+
+   const somekey = process.env.APPSETTING_somekey
+   const list= JSON.stringify(process.env)
+
+
+    res.send(list)
+})
+
+app.get('/api/merhaba', (req, res) => {
+
+    res.send('Merhaba Dünya')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+    console.log(body)
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running')
+})
